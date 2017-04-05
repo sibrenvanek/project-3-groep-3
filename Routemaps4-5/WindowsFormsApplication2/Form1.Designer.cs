7f644261468fd25a8c3d3f473f3e6fb6912a1d23
xkillererik <erik.van.berk@gmail.com>
2017-04-05 15:08:54 +0200

    Routemaps 4-5

---
+++ Routemaps4-5/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menu1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.filter1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.filter2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.filter3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 28);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1009, 492);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("", System.UriKind.Relative);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menu1ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1009, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menu1ToolStripMenuItem
+            // 
+            this.menu1ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.filter1ToolStripMenuItem,
+            this.filter2ToolStripMenuItem,
+            this.filter3ToolStripMenuItem});
+            this.menu1ToolStripMenuItem.Name = "menu1ToolStripMenuItem";
+            this.menu1ToolStripMenuItem.Size = new System.Drawing.Size(54, 24);
+            this.menu1ToolStripMenuItem.Text = "Filter";
+            this.menu1ToolStripMenuItem.Click += new System.EventHandler(this.menu1ToolStripMenuItem_Click);
+            // 
+            // filter1ToolStripMenuItem
+            // 
+            this.filter1ToolStripMenuItem.Name = "filter1ToolStripMenuItem";
+            this.filter1ToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.filter1ToolStripMenuItem.Text = "Filter1";
+            // 
+            // filter2ToolStripMenuItem
+            // 
+            this.filter2ToolStripMenuItem.Name = "filter2ToolStripMenuItem";
+            this.filter2ToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.filter2ToolStripMenuItem.Text = "Filter2";
+            // 
+            // filter3ToolStripMenuItem
+            // 
+            this.filter3ToolStripMenuItem.Name = "filter3ToolStripMenuItem";
+            this.filter3ToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.filter3ToolStripMenuItem.Text = "Filter3";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(191, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1009, 520);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menu1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem filter1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem filter2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem filter3ToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Routemaps4-5/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    
+    public partial class Form1 : Form
+    {
+        string url_standard = "https://www.bing.com/maps?rtp=";
+        
+        
+
+        string []url_list = new string[100];
+        
+        
+        public Form1()
+
+        {
+            
+            InitializeComponent();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void webBrowser2_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            
+
+        }
+
+        private void menu1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            url_list[0] = "~adr.Amsterdam";
+            url_list[1] = "~adr.Rotterdam,Zuid-Holland";
+            url_standard += url_list[0];
+            url_standard += url_list[1];
+            //url_list[].add("aaaa");
+            Console.WriteLine(url_list[2]);
+            
+
+            webBrowser1.Url = new System.Uri(url_standard);
+
+            /// add another button for this function
+            
+        }
+    }
+}
