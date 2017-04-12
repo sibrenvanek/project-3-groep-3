ea2faed7a4b0966c1b2f80770f3b35a0cc5540fb
sibrenvanek <0924655@hr.nl>
2017-04-12 09:44:46 +0200

    added base app

--- app monogame/app/BaseApp.cs
+++ app monogame/app/BaseApp.cs
@@ -11,13 +11,60 @@ namespace app
     class BaseApp
     {
         Texture2D Background;
-        public BaseApp(Texture2D background)
+        Texture2D Wijk2901;
+        Texture2D Wijk2902;
+        public bool W2901;
+        public bool W2902;
+        bool FPressed;
+        bool FHasBeenPressed;
+        public BaseApp(Texture2D background, Texture2D wijk2901, Texture2D wijk2902)
         {
             this.Background = background;
+            this.Wijk2901 = wijk2901;
+            this.Wijk2902 = wijk2902;
+            this.W2901 = true;
+            this.W2902 = true;
+        }
+        private void Switch2901()
+        {
+            if (W2901)
+                W2901 = false;
+            else if (!(W2901))
+                W2901 = true;
+        }
+        private void Switch2902()
+        {
+            if (W2902)
+                W2902 = false;
+            else if (!(W2902))
+                W2902 = true;
         }
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(this.Background, new Vector2(0,0), Color.White);
+            if(Microsoft.Xna.Framework.Input.Keyboard.GetState().IsKeyDown(Microsoft.Xna.Framework.Input.Keys.F))
+            {
+                FPressed = true;
+            }
+            if(FPressed)
+            {
+                FPressed = false;
+                FHasBeenPressed = true;
+            }
+            else if (FHasBeenPressed)
+            {
+                Switch2901();
+                Switch2902();
+                FHasBeenPressed = false;
+            }
+            spriteBatch.Draw(this.Background, new Vector2(0, 0), Color.White);
+            if (W2901)
+                spriteBatch.Draw(this.Wijk2901, new Vector2(0, 0), Color.Green);
+            else
+                spriteBatch.Draw(this.Wijk2901, new Vector2(0, 0), Color.Red);
+            if (W2902)
+                spriteBatch.Draw(this.Wijk2902, new Vector2(0, 0), Color.Green);
+            else
+                spriteBatch.Draw(this.Wijk2902, new Vector2(0, 0), Color.Red);
         }
     }
 }
