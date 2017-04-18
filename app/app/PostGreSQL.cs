2c7a14d6cdde35bd0307ad6e3b6b774455d48a82
HogeschoolNiels <0925927.hr.nl>
2017-04-18 11:30:00 +0200

    PostGreSQL

---
+++ app/app/PostGreSQL.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace app
+{
+    public class PostGreSQL
+    {
+        List<string> dataItems = new List<string>();
+
+        public void PostgreSQL()
+        {
+        }
+        public List<string> PostgreSQLtest1()
+        {
+            try
+            {
+                string connstring = "Server=127.0.0.1; Port=5432; User Id = postgres; Password=schijt123; Database=postgres;";
+                NpgsqlConnection connection = new NpgsqlConnection(connstring);
+                connection.Open();
+                NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM public.wijk", connection);
+                NpgsqlDataReader dataReader = command.ExecuteReader();
+                for (int i = 0; dataReader.Read(); i++)
+                {
+                    dataItems.Add(dataReader[0].ToString() + "," + dataReader[1].ToString() + "," + dataReader[2].ToString() + "\r\n");
+                }
+                connection.Close();
+                return dataItems;
+            }
+            catch (Exception msg)
+            {
+                MessageBox.Show(msg.ToString());
+                throw;
+            }
+        }
+    }
+}
